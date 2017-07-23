d2f528cfcbadd0b0cfe3936064cca386b6470acf
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-07-23 21:58:50 +0500

    Some code formatting

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -11,22 +11,21 @@ namespace AlgorithmsPractice
     {
         static void Main(String[] args)
         {
-            int numberOfRows = 5;
-            int numberOfColumns = 4;
+            int numberOfRows = 4;
+            int numberOfColumns = 5;
 
             int[,] randomNumbersMatrix = RandomNumberArrayGenerator.GetRandomNumber2DArray(-20, 20, numberOfRows, numberOfColumns);
-            MatirxPoint peak = new MatirxPoint { row = numberOfRows / 2, column = numberOfColumns / 2 };
 
             // to print the matrix
             for (int i = 0; i < numberOfRows; i++)
             {
                 Console.WriteLine();
                 for (int j = 0; j < numberOfColumns; j++)
-                    Console.Write(randomNumbersMatrix[i,j] + "   ");
+                    Console.Write(randomNumbersMatrix[i, j] + "   ");
             }
 
-            peak = PeakFinder.GetPeakOfTwoDimensionalArray(randomNumbersMatrix, peak);
-            Console.WriteLine(Environment.NewLine + Environment.NewLine + " " + peak.row + ", " + peak.column);
+            MatirxPoint peak = PeakFinder.GetPeakOfTwoDimensionalArray(randomNumbersMatrix);
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Peak at: (" + peak.row + ", " + peak.column + ")");
 
             Console.WriteLine();
         }
