23d7fcb9f5cdfe237cec9816bcbc6849558cd3b4
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-07-23 19:27:54 +0500

    Added functionality of 2D array peak finder
    May still have to handle some corner cases

--- AlgorithmsPractice/PeakFinder/PeakFinder.cs
+++ AlgorithmsPractice/PeakFinder/PeakFinder.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AlgorithmsPractice.PeakFinder
+namespace AlgorithmsPractice
 {
-    class PeakFinder
+    public struct MatirxPoint
+    {
+        public int row;
+        public int column;
+    }
+    public static class PeakFinder
     {
         public static int GetPeakOfOneDimensionalArray(int[] numbers, int min, int max)
         {
@@ -25,31 +30,48 @@ namespace AlgorithmsPractice.PeakFinder
             else return GetPeakOfOneDimensionalArray(numbers, middle + 1, max);
         }
 
-
-        public static int[,] GetRandomNumber2DArray(int minValue, int maxValue, int numberOfRows, int numberOfColumns)
+        public static MatirxPoint GetPeakOfTwoDimensionalArray(int[,] matrix, MatirxPoint currentPeak)
         {
-            int[,] matrix = new int[numberOfRows, numberOfColumns];
+            int selectedColumn = currentPeak.column;
+            int rowOfSelectedColumn = GetRowOfMaximumValueInAColumn(matrix, selectedColumn);
 
-            for (int i = 0; i < numberOfRows; i++)
+            if (matrix[rowOfSelectedColumn, selectedColumn] >= matrix[rowOfSelectedColumn, selectedColumn-1] && matrix[rowOfSelectedColumn, selectedColumn] >= matrix[rowOfSelectedColumn, selectedColumn + 1])
             {
-                int[] temp = GetRandomNumberArray(minValue, maxValue, numberOfColumns);
-                for (int j = 0; j < numberOfColumns; j++)
-                    matrix[i, j] = temp[j];
+                currentPeak.column = selectedColumn;
+                currentPeak.row = rowOfSelectedColumn;
+            }
+            else
+            {
+                currentPeak.column = currentPeak.column / 2;
+                GetPeakOfTwoDimensionalArray(matrix, currentPeak);
             }
 
-            return matrix;
+                return currentPeak;
         }
 
-        public static int[] GetRandomNumberArray(int min, int max, int size)
+        /// <summary>
+        /// Returns row index of the maximum value in a column
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="numberOfRows">Number of rows in the given matrix</param>
+        /// <param name="selectedColumn">The column from which the max value is to be searched</param>
+        /// <returns></returns>
+        private static int GetRowOfMaximumValueInAColumn(int[,] matrix, int selectedColumn)
         {
-            if (min >= max)
-                return null;
+            // improve this if possible
 
-            Random randomNumberGenerator = new Random();
-            int[] numbers = new int[size];
-            for (int i = 0; i < size; i++)
-                numbers[i] = randomNumberGenerator.Next(min, max);
-            return numbers;
+            int rowOfSelectedColumn = 0; ;
+            int numberOfRows = matrix.GetLength(0);
+            int maxInColumn = matrix[rowOfSelectedColumn, selectedColumn];
+
+            for (int i = 1; i < numberOfRows; i++)
+                if (matrix[i, selectedColumn] >= maxInColumn)
+                {
+                    maxInColumn = matrix[i, selectedColumn];
+                    rowOfSelectedColumn = i;
+                }
+
+            return rowOfSelectedColumn;
         }
     }
 }
