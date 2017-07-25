724a940dec99960211b8c79c546545997bab9487
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-07-26 00:32:38 +0500

    Skeleton code for document distance

---
+++ AlgorithmsPractice/DocumentDistance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice
+{
+    public static class DocumentDistance
+    {
+        public static long Find(string document1, string document2)
+        {
+            long distance = 0;
+
+
+            return distance;
+        }
+        
+
+        private static string[] GetWordsFromDocument(string document)
+        {
+            List<string> words = new List<string>();
+
+
+            return words.ToArray();
+        }
+
+        private static Dictionary<string, int> GetFrequenciesOfWords(string[] document)
+        {
+            Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+
+
+            return wordFrequencies;
+        }
+
+
+        private static long ComputeDotProductOfTwoDocuments()
+        {
+            long dotProduct = 0;
+
+
+            return dotProduct;
+        }
+    }
+}
