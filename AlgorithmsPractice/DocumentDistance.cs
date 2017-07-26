ee456038df48dda70210b42724069670668d1970
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-07-27 00:15:51 +0500

    Some functionalities added in finding document distance

--- AlgorithmsPractice/DocumentDistance.cs
+++ AlgorithmsPractice/DocumentDistance.cs
@@ -8,9 +8,16 @@ namespace AlgorithmsPractice
 {
     public static class DocumentDistance
     {
-        public static long Find(string document1, string document2)
+        public static long FindDistance(string document1, string document2)
         {
             long distance = 0;
+            string[] document1Words = GetWordsFromDocument(document1);
+            string[] document2Words = GetWordsFromDocument(document2);
+
+
+            Dictionary<string, int> document1WordsFrequencies = GetFrequenciesOfWords(document1Words);
+            Dictionary<string, int> document2WordsFrequencies = GetFrequenciesOfWords(document2Words);
+
 
 
             return distance;
@@ -21,6 +28,8 @@ namespace AlgorithmsPractice
         {
             List<string> words = new List<string>();
 
+            document = document.Replace("[-+,<.?/\\{}()!@#$%^&*]", " ");
+            words = document.Split(' ').ToList();
 
             return words.ToArray();
         }
@@ -28,6 +37,14 @@ namespace AlgorithmsPractice
         private static Dictionary<string, int> GetFrequenciesOfWords(string[] document)
         {
             Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+
+            foreach(string word in document)
+            {
+                if (wordFrequencies.ContainsKey(word))
+                    wordFrequencies[word]++;
+                else
+                    wordFrequencies.Add(word,0);
+            }
 
 
             return wordFrequencies;
