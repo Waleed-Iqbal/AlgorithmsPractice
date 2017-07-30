18449a25852588fcfb884bfe33e01ee8e19b0bc2
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-07-30 10:45:05 +0500

    skeleton for sorting algorithms

--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -11,19 +11,6 @@ namespace AlgorithmsPractice
     {
         static void Main(String[] args)
         {
-            string firstDocumentPath = @"D:\Projects\AlgorithmsPractice\AlgorithmsPractice\Resources\t8.shakespeare.txt";
-            string secondDocumentPath = @"D:\Projects\AlgorithmsPractice\AlgorithmsPractice\Resources\t9.bacon.txt";
-            double computedDistance = DocumentDistance.FindDistance(firstDocumentPath, secondDocumentPath);
-
-            Console.WriteLine(Environment.NewLine + " Distance computed = " + computedDistance);
-            Console.WriteLine(Environment.NewLine + " Actual distance = 0.52464214746385918");
-
-            /*
-            Document distance of 't1.verne.txt' and 't2.bobsey.txt' is 0.53338963892153224
-            Document distance of 't2.bobsey.txt' and 't9.bacon.txt' is 0.52464214746385918
-            Document distance of 't8.shakespeare.txt' and 't9.bacon.txt' is 0.55090929545369682
-            */
-
 
 
             Console.WriteLine();
---
+++ AlgorithmsPractice/Sorting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsPractice
+{
+    public static class Sorting
+    {
+        public static int[] InsertionSort(int[] numbers)
+        {
+
+
+
+            return numbers;
+        }
+
+
+
+        public static int[] MergeSort(int[] numbers)
+        {
+
+
+
+            return numbers;
+        }
+    }
+}
