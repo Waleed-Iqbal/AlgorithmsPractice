a709ddef90017e3e562c8edf52fdb71bda6821ee
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2017-08-05 21:03:01 +0500

    Building MaxHeap working fine now.

--- AlgorithmsPractice/Sorting/HeapSort.cs
+++ AlgorithmsPractice/Sorting/HeapSort.cs
@@ -10,18 +10,35 @@ namespace AlgorithmsPractice
         /// <returns></returns>
         public static void Sort(int[] numbers)
         {
+            BuildMaxHeap(numbers);
 
-        }
-
-        private static int[] MaxHeapify()
-        {
-            int[] numbers = { };
-
-            return numbers;
+            return;
         }
 
         private static void BuildMaxHeap(int[] numbers)
         {
+            for (int i = numbers.Length / 2 - 1; i >= 0; i--)
+            {
+                MaxHeapify(numbers, i);
+            }
+        }
+
+        private static void MaxHeapify(int[] numbers, int index)
+        {
+            int leftNodeIndex = index * 2;
+            int rightNodeIndex = leftNodeIndex + 1;
+            int largest = index;
+            if (leftNodeIndex < numbers.Length && numbers[leftNodeIndex] > numbers[index] )
+                largest = leftNodeIndex;
+            if (rightNodeIndex < numbers.Length && numbers[rightNodeIndex] > numbers[largest])
+                largest = rightNodeIndex;
+            if (largest != index)
+            {
+                numbers[index] = numbers[index] + numbers[largest];
+                numbers[largest] = numbers[index] - numbers[largest];
+                numbers[index] = numbers[index] - numbers[largest];
+                MaxHeapify(numbers, largest);
+            }
         }
     }
 }
