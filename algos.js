f8bbe0fd166e27e8e240df8732b176a0e47e942a
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-13 01:21:38 +0500

    remove dot net stuff and use js instead and maybe python

---
+++ algos.js
@@ -0,0 +1,23 @@
+
+
+
+(function(){
+  
+  document.getElementById('output').value = "OUTPUT\n---------\n\n\n";
+  appendOutput = (text) => {
+    document.getElementById('output').value  += text;
+  }
+  
+  
+  
+  firstOne = ()=> {
+    appendOutput("result");
+  }
+  
+  
+  algos = {
+    firstOne:firstOne
+  };
+  
+  algos.firstOne();
+}())
