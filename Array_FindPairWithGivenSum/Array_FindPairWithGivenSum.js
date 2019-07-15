4143cc36128fbd2a00b26a40a4103afce7996ab0
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-16 00:30:09 +0500

    use map and display execution time

--- Array_FindPairWithGivenSum/Array_FindPairWithGivenSum.js
+++ Array_FindPairWithGivenSum/Array_FindPairWithGivenSum.js
@@ -11,33 +11,37 @@
     sum: 10
   };
 
-
   let bruteForceSolution = () => {
+    console.log("BRUTE FORCE SOLUTION ...");
+
     let numbers = testInput1.numbers;
     let sum = testInput1.sum;
     let isPairFound = false;
-    
-    for (let i = 0; i < numbers.length; i++) {
-      
-      const comparator = numbers[i];
-      for (let j = i + 1; j < numbers.length; j++) {
-        
+
+    let timeStart = performance.now();
+    numbers.map((comparator, index) => {
+      for (let j = index + 1; j < numbers.length; j++) {
+
         const comparer = numbers[j];
-        
+
         if (sum === comparator + comparer) {
-          console.log(`Sum found at indices ${i} and ${j}. Values are ${numbers[i]}, ${numbers[j]}`);
+          console.log(`Sum found at indices ${index} and ${j}. Values are ${comparator}, ${numbers[j]}`);
           isPairFound = true;
           break;
         }
       }
-    }
-    
-    if(isPairFound) {
+    });
+
+    let timeEnd = performance.now();
+    let totalTimeTaken = parseFloat(timeEnd - timeStart).toFixed(5);
+
+    if (isPairFound) {
       console.log(`Brute force time complexity: O(n^2)`);
       console.log(`Brute force space complexity: O(1)`);
     }
+    console.log(`Time taken: ${totalTimeTaken} milliseconds`);
   }
-  
+
   bruteForceSolution();
 
 })();
