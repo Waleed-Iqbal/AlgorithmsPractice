a16d88eab351b4074ed33f0626f64214fd327bad
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-16 00:15:21 +0500

    add time and space complexity

--- Array_FindPairWithGivenSum/Array_FindPairWithGivenSum.js
+++ Array_FindPairWithGivenSum/Array_FindPairWithGivenSum.js
@@ -6,29 +6,35 @@
 
 (() => {
 
-  let testTnput1 = {
+  let testInput1 = {
     numbers: [8, 7, 2, 5, 3, 1],
     sum: 10
   };
 
 
   let bruteForceSolution = () => {
-    let numbers = testTnput1.numbers;
-    let sum = testTnput1.sum;
+    let numbers = testInput1.numbers;
+    let sum = testInput1.sum;
+    let isPairFound = false;
     
     for (let i = 0; i < numbers.length; i++) {
       
       const comparator = numbers[i];
-      
       for (let j = i + 1; j < numbers.length; j++) {
         
         const comparer = numbers[j];
         
         if (sum === comparator + comparer) {
-          console.log(`Sum found at index ${i} and ${j}`);
+          console.log(`Sum found at indices ${i} and ${j}. Values are ${numbers[i]}, ${numbers[j]}`);
+          isPairFound = true;
           break;
         }
       }
+    }
+    
+    if(isPairFound) {
+      console.log(`Brute force time complexity: O(n^2)`);
+      console.log(`Brute force space complexity: O(1)`);
     }
   }
   
