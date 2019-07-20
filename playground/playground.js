58b05e8240e734158e76ef1950778d9cd1206a07
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-20 21:22:53 +0500

    study arrow function as object methods

--- playground/playground.js
+++ playground/playground.js
@@ -1,12 +1,24 @@
 
 (() => {
 
+  let testArrow = {
+
+    data: "address",
+
+    methodOld: function() {
+      console.log(`old ${this.data}`);
+    },
+
+    methodNew: () => {
+      return () => {
+
+        console.log(`new ${testArrow.data}`);
+      };
+    }
+  };
 
 
-
-
-
-
-
+  testArrow.methodOld();
+  testArrow.methodNew()();
 
 })();
