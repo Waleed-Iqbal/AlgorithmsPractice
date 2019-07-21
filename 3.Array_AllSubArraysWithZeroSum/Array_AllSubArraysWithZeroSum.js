670eca95f30989e64c376140ca36d0b3e6530728
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-22 01:13:53 +0500

    all subarrays with zero sum

--- 3.Array_AllSubArraysWithZeroSum/Array_AllSubArraysWithZeroSum.js
+++ 3.Array_AllSubArraysWithZeroSum/Array_AllSubArraysWithZeroSum.js
@@ -6,15 +6,20 @@
 
 (() => {
 
+  let showLog = globals.showLog;
+  let testASolution = globals.testASolution;
 
-  let testInput1 = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2];
+
+  // let testInput1 = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2];
+  let testInput1 = globals.testData.numbers;
   // Sub arrays are
   // [3,4,-7]
+  // [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
   // [4,-7,3]
   // [-7,3,1,3]
-  // [3,1,-4]
   // [3,1,3,1,-4,-2,-2]
-  // []
+  // [3,1,-4]
+
 
   let testInput2 = [4, 2, -3, -1, 0, 4];
   // sub arrays are
@@ -22,12 +27,32 @@
   // [0]
 
 
-  let bruteForceSolution = (numbers) => {
+  // O(n^2)
+  let bruteForceSolution = (showOutputs) => {
 
+    let sum = 0;
+    let subArray = "";
+    let totalNumbers = testInput1.length;
 
+    testInput1.map((number, index, numbers) => {
+      sum = 0;
+      subArray = "";
 
+      for (let i = index; i < totalNumbers; i++) {
+        sum += numbers[i];
+        subArray += numbers[i] + ", ";
+
+        if (sum === 0)
+          showLog(`ZERO SUM SUBARRAY: ${subArray}`, showOutputs);
+      }
+    });
   }
 
-  allSubArrayWithZeroSumExist(testInput1);
+  let displayOptions = {
+    showInputs: false,
+    showOutputs: true
+  }
+
+  testASolution(bruteForceSolution, { showInputs: true, showOutputs: displayOptions.showOutputs }); // O(n^2)
 
 })();
