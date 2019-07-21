1a30c8d1ee9b81d8e4b41aaf4cf906f4286828f3
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-21 23:09:13 +0500

    update names

---
+++ 3.Array_AllSubArraysWithZeroSum/Array_AllSubArraysWithZeroSum.js
@@ -0,0 +1,33 @@
+
+
+// taken from
+// https://www.techiedelight.com/check-subarray-with-0-sum-exists-not/
+
+
+(() => {
+
+
+  let testInput1 = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2];
+  // Sub arrays are
+  // [3,4,-7]
+  // [4,-7,3]
+  // [-7,3,1,3]
+  // [3,1,-4]
+  // [3,1,3,1,-4,-2,-2]
+  // []
+
+  let testInput2 = [4, 2, -3, -1, 0, 4];
+  // sub arrays are
+  // [-3,-1,0,4]
+  // [0]
+
+
+  let allSubArrayWithZeroSumExist = (numbers) => {
+
+
+
+  }
+
+  allSubArrayWithZeroSumExist(testInput1);
+
+})();
