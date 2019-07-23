93030831ee1dddb36e1e8a76c46b65dadfda5aaa
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-07-23 23:18:54 +0500

    add template for each new problem

---
+++ new problem template/new problem template.js
@@ -0,0 +1,20 @@
+
+
+// taken from
+// https://www.techiedelight.com/check-subarray-with-0-sum-exists-not/
+
+
+(() => {
+
+  let showLog = globals.showLog;
+  let testASolution = globals.testASolution;
+
+
+  let displayOptions = {
+    showInputs: false,
+    showOutputs: true
+  }
+
+  testASolution(fun_name, test_input, { showInputs: true, showOutputs: displayOptions.showOutputs });
+
+})();
