200e60963adb87ee0ecfff33960edfc656da566a
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-11-30 00:27:26 +0500

    Display inputs and outputs

--- 4.Array_SortBinaryArrayInLinearTime/Array_SortBinaryArrayInLinearTime.js
+++ 4.Array_SortBinaryArrayInLinearTime/Array_SortBinaryArrayInLinearTime.js
@@ -10,44 +10,32 @@
   let testASolution = globals.testASolution;
 
 
-  // let testInput1 = {
-  //   numbers: [0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1]
-  // };
+  let testInput = {
+    numbers: [0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1]
+  };
 
-  let testInput1 = [0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1];
-
-  let bruteForceSolution = (input) => {
+  let linearSolution = (showOutputs) => {
     // if 0 is found, prepend it to new array, for 1, append it to array
 
-    let log = `Input:\n`;
     let sortedBirnayrArray = [];
-    input.map(value => {
-      log += `${value}, `;
+    let log = "";
 
+    testInput.numbers.map(value => {
       if (value == 1)
         sortedBirnayrArray.push(value);
       else
         sortedBirnayrArray.unshift(value);
     });
 
-    log += `\nOutput:\n`;
-
-    sortedBirnayrArray.map(value => {
-      log += `${value}, `;
-    })
-
-
-    console.log(log);
-    return sortedBirnayrArray;
-
+    showLog(`SORTED BINARY ARRAY: ${log}`, showOutputs);
   }
 
   let displayOptions = {
-    showInputs: false,
+    showInputs: true,
     showOutputs: true
   }
 
-  bruteForceSolution(testInput1);
-  //testASolution(bruteForceSolution, testInput1);
+  linearSolution(testInput);
+  testASolution(linearSolution, testInput, displayOptions);
 
 })();
