7dfdf641c90d01a2b7a3afe1c5d0c77684050165
waleed-iqbal <ikhawajawaleed@gmail.com>
2019-12-02 00:11:31 +0500

    add fifth problem

---
+++ 5.Array_FindDuplicateElementInLimitedRange/Array_FindDuplicateElementInLimitedRange.js
@@ -0,0 +1,20 @@
+
+
+// taken from
+// https://www.techiedelight.com/find-duplicate-element-limited-range-array/
+
+
+(() => {
+
+  let showLog = globals.showLog;
+  let testASolution = globals.testASolution;
+
+
+  let displayOptions = {
+    showInputs: false,
+    showOutputs: true
+  }
+
+  testASolution(fun_name, test_input, { showInputs: true, showOutputs: displayOptions.showOutputs });
+
+})();
