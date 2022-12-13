64a89f903e92b0cb6c4c358444a280e0891ee227
waleed-iqbal <ikhawajawaleed@gmail.com>
2022-12-13 23:55:39 +0500

    should have worked, but didn't

--- advent-of-code-2022/day-8/solution-8.py
+++ advent-of-code-2022/day-8/solution-8.py
@@ -9,7 +9,6 @@ grid = [[] for x in range(len(formatted_input))]
 for index in range(0, len(grid)):
   grid[index] = [int(x) for x in formatted_input[index]]
 
-text = ''
 total_length = len(grid)
 is_tree_visible = False
 total_visible_trees = 0
@@ -21,38 +20,45 @@ def checkForTreeVisibility():
 
   if is_tree_visible:
     total_visible_trees += 1
-    is_tree_visible = False
 
-for i in range(1, total_length - 1):
+
+
+for i in range(1, total_length):
   row = grid[i]
 
-  for j in range(1, len(row) - 1):
+  for j in range(1, len(row)):
     current_tree = row[j]
 
     # towards top edge
     for top in range(i-1, -1, -1):
       is_tree_visible = current_tree > grid[top][j]
       if not is_tree_visible: break
-
-    checkForTreeVisibility()
+    if is_tree_visible:
+      total_visible_trees += 1
+      continue
 
     # towards right edge
     for right in range(j+1, len(row)):
       is_tree_visible = current_tree > grid[i][right]
       if not is_tree_visible: break
-    checkForTreeVisibility()
+    if is_tree_visible:
+      total_visible_trees += 1
+      continue
 
     # towards bottom edge
     for bottom in range(i+1, total_length):
       is_tree_visible = current_tree > grid[bottom][j]
       if not is_tree_visible: break
-    checkForTreeVisibility()
+    if is_tree_visible:
+      total_visible_trees += 1
+      continue
 
     # towards left edge
     for left in range(j-1, -1, -1):
       is_tree_visible = current_tree > grid[i][left]
       if not is_tree_visible: break
-    checkForTreeVisibility()
+    if is_tree_visible:
+      total_visible_trees += 1
 
 
 total_visible_trees += (total_length * 4 ) - 4
