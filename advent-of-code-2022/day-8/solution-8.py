e485ab5aa3fe1e99e8a67c6d5c98d880fcaac008
waleed-iqbal <ikhawajawaleed@gmail.com>
2022-12-14 01:18:09 +0500

    attemp for part 2

--- advent-of-code-2022/day-8/solution-8.py
+++ advent-of-code-2022/day-8/solution-8.py
@@ -6,6 +6,7 @@ pprint = pprint.pprint
 formatted_input = open(Path(__file__).with_name("input-8.txt").absolute(), "r").read().split('\n')
 
 grid = [[] for x in range(len(formatted_input))]
+
 for index in range(0, len(grid)):
   grid[index] = [int(x) for x in formatted_input[index]]
 
@@ -13,7 +14,7 @@ total_length = len(grid)
 is_tree_visible = False
 total_visible_trees = 0
 
-
+"""
 def checkForTreeVisibility():
   global total_visible_trees
   global is_tree_visible
@@ -60,3 +61,50 @@ for i in range(1, total_length - 1):
 
 total_visible_trees += (total_length * 4 ) - 4
 # 1708 is correct
+"""
+
+# part 2
+scenic_distances = []
+
+for i in range(0, total_length):
+  row = grid[i]
+
+  for j in range(0, len(row)):
+    current_tree = row[j]
+
+    distance_top = 1
+    distance_right = 1
+    distance_bottom = 1
+    distance_left = 1
+
+    # towards top edge
+    for top in range(i - 1, -1, -1):
+      if current_tree == grid[top][j]: break
+      if current_tree > grid[top][j]: distance_top += 1
+
+    # towards right edge
+    for right in range(j + 1, len(row)):
+      if current_tree == grid[i][right]: break
+      if current_tree > grid[i][right]: distance_right += 1
+
+    # towards bottom edge
+    for bottom in range(i + 1, total_length):
+      if current_tree == grid[bottom][j]: break
+      if current_tree > grid[bottom][j]: distance_bottom += 1
+
+    # towards left edge
+    for left in range(j - 1, -1, -1):
+      if current_tree == grid[i][left]: break
+      if current_tree > grid[i][left]: distance_left += 1
+
+    scenic_distances.append(distance_top * distance_right * distance_bottom * distance_left)
+
+
+print(max(scenic_distances))
+
+"""
+Guesses
+
+56925 - too low
+562275 - too high
+"""
