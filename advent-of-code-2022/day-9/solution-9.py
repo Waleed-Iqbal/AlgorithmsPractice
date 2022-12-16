db65aff497560a7c9ac9b12e7d590e51b9497735
waleed-iqbal <ikhawajawaleed@gmail.com>
2022-12-16 22:36:20 +0500

    add helper functions

--- advent-of-code-2022/day-9/solution-9.py
+++ advent-of-code-2022/day-9/solution-9.py
@@ -1,4 +1,5 @@
 import pprint
+import math
 from pathlib import Path
 
 pprint = pprint.pprint
@@ -6,3 +7,20 @@ pprint = pprint.pprint
 formatted_input = open(Path(__file__).with_name("input-9.txt").absolute(), "r").read().split('\n')
 
 print(formatted_input)
+
+
+def find_distance(pos_h, pos_t):
+  return  math.sqrt(math.pow(pos_h.x - pos_t.x, 2))
+
+def isSameRow(pos_h, pos_t):
+  return pos_h.x == poh_t.x
+
+def isSameColumn(pos_h, pos_t):
+  return pos_h.y == poh_t.y
+
+def isDiagonal(pos_h, pos_t):
+  return not isSameRow(pos_h, pos_t) and not isSameColumn(pos_h, pos_t)
+
+
+head_pos = {x: 0, y: 0}
+tail_pos = {x: 0, y: 0}
