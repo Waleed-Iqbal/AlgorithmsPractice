c366867278c90334ef6c771ee5ebf161638ece75
waleed-iqbal <ikhawajawaleed@gmail.com>
2022-12-18 23:50:26 +0500

    debugging is awn

--- advent-of-code-2022/day-9/solution-9.py
+++ advent-of-code-2022/day-9/solution-9.py
@@ -6,21 +6,19 @@ pprint = pprint.pprint
 
 formatted_input = open(Path(__file__).with_name("input-9.txt").absolute(), "r").read().split('\n')
 
-# print(formatted_input)
-
 head_pos = {'x': 0, 'y': 0}
 tail_pos = {'x': 0, 'y': 0}
 reference = 0
-tail_visit_pos = []
+tail_visit_pos = {}
 
 def get_distance(pos_h, pos_t):
   return  math.sqrt(math.pow(pos_h['x'] - pos_t['x'], 2) + math.pow(pos_h['y'] - pos_t['y'], 2) )
 
 def isSameRow(pos_h, pos_t):
-  return pos_h['x'] == pos_t['x']
+  return pos_h['y'] == pos_t['y']
 
 def isSameColumn(pos_h, pos_t):
-  return pos_h['y'] == pos_t['y']
+  return pos_h['x'] == pos_t['x']
 
 def isDiagonal(pos_h, pos_t):
   return not isSameRow(pos_h, pos_t) and not isSameColumn(pos_h, pos_t)
@@ -36,19 +34,46 @@ for step in formatted_input:
   is_down = direction == 'D'
   is_left = direction == 'L'
 
-  if is_up: head_pos['y'] += movement
-  elif is_right: head_pos['x'] += movement
-  elif is_down: head_pos['y'] -= movement
-  elif is_left: head_pos['x'] -= movement
+  print(f" ---- NEXT STEP  {step} ----")
 
-  distance = get_distance(head_pos, tail_pos)
+  for step_distance in range(1, movement + 1):
+    if is_up: head_pos['y'] += 1
+    elif is_right: head_pos['x'] += 1
+    elif is_down: head_pos['y'] -= 1
+    elif is_left: head_pos['x'] -= 1
 
-  isTouching = distance < 2
+    distance = get_distance(head_pos, tail_pos)
+    isTouching = distance < 2
 
-  if isTouching: continue
-  else:
-    print('AHEAD')
-    print(f'H: {head_pos} - T: {tail_pos} - Distance: {distance}')
+    if isTouching: continue # don't need an else as that means not touching
+
+    if isSameRow(head_pos, tail_pos):
+      if is_right: tail_pos['x'] += 1
+      elif is_left: tail_pos['x'] -= 1
+      print(f'same row : Head({head_pos["x"]}, {head_pos["y"]}) -- Tail({tail_pos["x"]}, {tail_pos["y"]})')
+    elif isSameColumn(head_pos, tail_pos):
+      if is_up: tail_pos['y'] += 1
+      elif is_down: tail_pos['y'] -= 1
+      print(f'same col : Head({head_pos["x"]}, {head_pos["y"]}) -- Tail({tail_pos["x"]}, {tail_pos["y"]})')
+    else:
+      if is_up or is_right: 
+        tail_pos['y'] += 1
+        tail_pos['x'] += 1
+      elif is_down or is_left:
+        tail_pos['y'] -= 1
+        tail_pos['x'] -= 1
+      print(f'diagonal : Head({head_pos["x"]}, {head_pos["y"]}) -- Tail({tail_pos["x"]}, {tail_pos["y"]})')
+
+    tail_visit_pos[f'{tail_pos["x"]}--{tail_pos["y"]}'] = 'visited'
+    print('----------------------')
+
+# print(tail_visit_pos)
+print(len(tail_visit_pos.keys()))
 
 
-
+"""
+Guesses
+1. 7178 - too high
+2. 817 - too low
+3. 1214 - too low
+"""
