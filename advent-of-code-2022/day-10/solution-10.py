99a8795e4c11640c7e184a1c30d0587b2e5da692
waleed-iqbal <ikhawajawaleed@gmail.com>
2022-12-19 03:02:45 +0500

    advent of code day 10 - scaffolding

---
+++ advent-of-code-2022/day-10/solution-10.py
@@ -0,0 +1,9 @@
+import pprint
+import math
+from pathlib import Path
+
+pprint = pprint.pprint
+
+formatted_input = open(Path(__file__).with_name("input-10.txt").absolute(), "r").read().split('\n')
+
+print(formatted_input)
