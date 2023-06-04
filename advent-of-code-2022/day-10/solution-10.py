c2f3f4c1a18a04d4b2e4f9e9b8aa136172d6769e
waleed-iqbal <ikhawajawaleed@gmail.com>
2023-06-04 17:36:36 +0500

    day 10 - attempt 1

--- advent-of-code-2022/day-10/solution-10.py
+++ advent-of-code-2022/day-10/solution-10.py
@@ -6,4 +6,44 @@ pprint = pprint.pprint
 
 formatted_input = open(Path(__file__).with_name("input-10.txt").absolute(), "r").read().split('\n')
 
-print(formatted_input)
+total_cycles = 0
+calcualted_x = 1
+signal_strength = 0
+
+def calculate_strength():
+  global total_cycles
+  global calcualted_x
+  global signal_strength
+
+  total_cycles = total_cycles + 1
+
+  if total_cycles == 20:
+    signal_strength = total_cycles * calcualted_x
+    print(
+      total_cycles,
+      calcualted_x,
+      signal_strength
+    )
+
+  elif total_cycles % 60 == 0:
+    signal_strength = signal_strength + (total_cycles * calcualted_x)
+    print(
+      total_cycles,
+      calcualted_x,
+      signal_strength
+    )
+
+for operation in formatted_input:
+    if operation == 'noop':
+      calculate_strength()
+
+    elif operation.startswith('addx'):
+      for i in range(0, 2):
+        calculate_strength()
+      calcualted_x = calcualted_x + int(operation.split(' ')[1])
+        
+
+    
+      
+
+    
